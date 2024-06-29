1c0a0a52da561fb6c95668963b896fa914c85ef3
uflilla <uflilla@gmail.com>
2024-06-29 19:43:03 +0500

    Add project files.

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILoggerManager _logger;
+
+        public WeatherForecastController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInfo("Here is info message from our values controller.");
+            _logger.LogDebug("Here is debug message from our values controller.");
+            _logger.LogWarn("Here is warn message from our values controller.");
+            _logger.LogError("Here is an error message from our values controller.");
+            return new string[] { "value1", "value2" };
+        }
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
