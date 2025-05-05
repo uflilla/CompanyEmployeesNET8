f3dd372981ede5985910334873eda9a20e6bdc45
uflilla <uflilla@gmail.com>
2025-05-05 21:54:23 +0500

    Searching Chaper 18 completed.

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Contracts;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 using Shared.RequestFeatures;
 
 namespace Repository;
@@ -13,10 +14,11 @@ internal sealed class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRe
     }
     public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
     {
-        var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && (e.Age >= employeeParameters.MinAge && e.Age <=employeeParameters.MaxAge), trackChanges)
+        var employees = await FindByCondition(e => e.CompanyId.Equals(companyId),
+                trackChanges)
+            .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+            .Search(employeeParameters.SearchTerm)
             .OrderBy(e => e.Name)
-            .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
-            .Take(employeeParameters.PageSize)
             .ToListAsync();
         var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
         return new PagedList<Employee>(employees, count, employeeParameters.PageNumber, employeeParameters.PageSize);
